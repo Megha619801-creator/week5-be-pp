d2c707278abe199c14ae5a212061aab59f70ecf7
Megha619801-creator <mahatomahi2062@gmail.com>
2025-09-21 16:30:53 +0300

    Refactor tours API to use MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
+connectDB();
+
 // Middleware to parse JSON
 app.use(express.json());
- 
+
 // Use the tourRouter for all "/tours" routes
 app.use("/api/tours", tourRouter);
 
@@ -17,11 +21,18 @@ app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
-// app.use(errorHandler);
+app.use(errorHandler);
+
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Network problem");
+  next(error);
+});
 
 const port = process.env.PORT || 4000;
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
- 
+  
